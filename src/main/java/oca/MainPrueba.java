706cc5f0912a7b2268c04ae72aa898a46322b916
alopezp90 <alopezp90@gmail.com>
2020-12-20 22:29:57 +0100

    Pruebas con botones

--- src/main/java/oca/MainPrueba.java
+++ src/main/java/oca/MainPrueba.java
@@ -4,8 +4,33 @@ package oca;
  * @author Alberto López Puertas
  * <alopezp90@gmail.com>
  */
+import javax.swing.*;
+import java.awt.*;
+
 public class MainPrueba {
 
     public static void main(String[] args) {
+
+        int alto = 100;
+        int ancho = 366;
+
+        JFrame ventana = new JFrame();
+        ventana.setResizable(false);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.getContentPane();
+        ventana.setSize(366, alto + 36);
+
+        JButton boton = new JButton("Tirar");
+        boton.setBounds(133, 0, 100, 100);
+
+        JPanel panel = new JPanel();
+        panel.setSize(366, alto);
+        panel.setLayout(null);
+        panel.add(boton);
+
+        ventana.setLayout(null);
+        ventana.add(panel);
+
+        ventana.setVisible(true);
     }
 }
