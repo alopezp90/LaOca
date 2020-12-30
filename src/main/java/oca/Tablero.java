6f23a457211360886bb4c766e99301137602afdf
alopezp90 <alopezp90@gmail.com>
2020-12-30 01:17:13 +0100

    Documenta la clase Tablero y añade parametro posicion y point a Casilla

--- src/main/java/oca/Tablero.java
+++ src/main/java/oca/Tablero.java
@@ -1,55 +1,64 @@
 package oca;
 
+
 /**
+ * Una clase para definir el array de casillas que forma el tablero de La Oca.
+ * Construye un array de 64 casillas con la disposicion de La Oca.
+ * 
  * @author Alberto López Puertas
- * <alopezp90@gmail.com>
+ * <https://github.com/alopezp90>
  */
+
+import java.awt.Point;
+
 public class Tablero {
 
+    //Instancia variables
     private Casilla[] casilla;
 
+    /**
+     * Constructor por defecto de Tablero.
+     * Recorre el array y crea un objeto Casilla adecuado en cada posicion.
+     */
     public Tablero() {
 
         this.casilla = new Casilla[64];
         
+        //Recorre el array creando la casilla adecuada en cada posicion
         casilla[0] = new Casilla("salida");
-
-        for (int i = 0; i < 64; i++) {
+        for (int i = 1; i < 64; i++) {
             switch (i) {
                 case 5:
-                case 14:
-                case 23:
-                case 32:
-                case 41:
-                case 50:
-                case 59:
-                    casilla[i] = new Casilla("oca1");
-                    break;
                 case 9:
+                case 14:
                 case 18:
+                case 23:
                 case 27:
+                case 32:
                 case 36:
+                case 41:
                 case 45:
+                case 50:
                 case 54:
-                    casilla[i] = new Casilla("oca2");
+                case 59:
+                    casilla[i] = new Casilla("oca");
                     break;
                 case 6:
-                    casilla[i] = new Casilla("puente1");
-                    break;
                 case 12:
-                    casilla[i] = new Casilla("puente2");
+                    casilla[i] = new Casilla("puente");
+                    break;
+                case 19:
+                    casilla[i] = new Casilla("posada");
                     break;
                 case 26:
-                    casilla[i] = new Casilla("dados1");
+                case 53:
+                    casilla[i] = new Casilla("dados");
                     break;
                 case 31:
                     casilla[i] = new Casilla("pozo");
                     break;
                 case 42:
                     casilla[i] = new Casilla("laberinto");
-                    break;
-                case 53:
-                    casilla[i] = new Casilla("dados2");
                     break;
                 case 56:
                     casilla[i] = new Casilla("carcel");
@@ -61,19 +70,35 @@ public class Tablero {
                     casilla[i] = new Casilla();
                     break;
             }
-            if (i != 0){
-                casilla[i].setPosicionY(50 + (int) (i/9) * 100);
-
-                if (i/9 % 2 == 0){
-                    casilla[i].setPosicionX(50 + 100 * (i - (9 * (int) (i/9))));
-                } else {
-                    casilla[i].setPosicionX(850 - 100 * (i - (9 * (int) (i/9))));
-                }
+        }
+        
+        //Recorre el array dando valor a posicion y point de cada casilla
+        for (int i = 1; i < 64; i++) {
+            //Calcula x e y temporales
+            int x;
+            if (Math.ceil(i/9.0) % 2 != 0){  //En las filas pares
+                x = (int) (20 + (100 * ((i - Math.floor((i - 1)/9.0) * 9) - 1)));
+            } else {                    //En las filas impares
+                x = (int) (820 - (100 * ((i - Math.floor((i - 1)/9.0) * 9) - 1)));
             }
+            int y = (int) (715 - (100 * Math.ceil(i/9.0)));  //Hay que corregir 5px hacia arriba (?)
+            
+            //Da los valores buscados a casilla
+            casilla[i].setPosicion(i);
+            casilla[i].setPoint(new Point(x, y));   
         }
     }
 
-    public Casilla[] getCasilla() {
-        return casilla;
+    public Casilla getCasilla(int i) {
+        return casilla[i];
+    }
+    
+    /**
+     * Metodo que cuenta el numero de casillas hasta la siguiente oca.
+     * @param i elemento de tablero desde el que empieza a contar.
+     * @return n numero de casillas hasta la siguiente oca.
+     */
+    public int nextOca(int i) {
+        return 0;
     }
 }
