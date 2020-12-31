34f53f4f6b276fd2ae106112bc587f91664363d3
alopezp90 <alopezp90@gmail.com>
2020-12-31 11:30:45 +0100

    Añade VentanaGanador

---
+++ src/main/java/oca/gui/VentanaGanador.java
@@ -0,0 +1,72 @@
+package oca.gui;
+
+/**
+ * Esta clase define la ventana que da la enhorabuena al ganador del juego.
+ *
+ * @author Alberto López Puertas
+ * <https://github.com/alopezp90>
+ */
+import java.awt.FlowLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class VentanaGanador extends JFrame {
+
+    //Declara constantes
+    private final ImageIcon FONDO = new ImageIcon("src/main/resources/FondoAragon.png");
+    private final String rutaRotulo = "src/main/resources/rotuloX.png";
+    private final int ANCHO = 656, ALTO = 370;
+    private final int ANCHO_ROTULO = 504, ALTO_ROTULO = 73;
+    private final String titulo = "La Oca";
+
+    //Inicializa las variables
+    private JLabel fondo = new JLabel();
+    private JLabel rotuloLabel = new JLabel();
+    private JPanel rotuloPanel = new JPanel();
+
+    //Instancia las variables
+    private ImageIcon rotulo;
+
+    /**
+     * Constructor parametrizado de la clase.
+     *
+     * @param i numero del jugador ganador
+     */
+    public VentanaGanador(int i) {
+
+        //Parametros del JFrame
+        this.setTitle(titulo);
+        this.setSize(ANCHO, ALTO);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        //Configura imagen de fondo
+        fondo.setLayout(new FlowLayout());
+        fondo.setIcon(FONDO);
+        fondo.setLayout(null);
+
+        //Define cual sera el rotulo
+        rotulo = new ImageIcon(rutaRotulo.replaceAll("X", "" + i));
+
+        //Configura la imagen del rotulo
+        rotuloLabel.setIcon(rotulo);
+
+        //Configura el panel del rotulo
+        rotuloPanel.setBounds((ANCHO - ANCHO_ROTULO) / 2, 250, ANCHO_ROTULO, ALTO_ROTULO + 5);
+        rotuloPanel.setOpaque(false);
+        rotuloPanel.setLayout(new FlowLayout());
+        
+        //Añade el rotulo en su panel, el panel al fondo y el fondo al frame
+        rotuloPanel.add(rotuloLabel);
+        fondo.add(rotuloPanel);
+        this.add(fondo);
+    }
+    
+    public static void main(String[] args) {
+        VentanaGanador frame = new VentanaGanador(1);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
