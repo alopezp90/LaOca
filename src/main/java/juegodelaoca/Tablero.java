8b15e092c45a1ee1bcc8ba1be544ffa15f44071d
alopezp90 <alopezp90@gmail.com>
2021-02-21 13:11:06 +0100

    En progreso - Adaptando Tablero a ultimo UML

--- src/main/java/juegodelaoca/Tablero.java
+++ src/main/java/juegodelaoca/Tablero.java
@@ -13,40 +13,25 @@ public class Tablero {
 
     //Instancia variables
     private Casilla[] tablero;
+    
+    //Almacena la posicion del Pozo para que pueda ser leida por otras clases
+    public static final int POS_POZO = 31;
 
     /**
      * Constructor por defecto de Tablero. Recorre el array y crea un objeto
      * Casilla adecuado en cada posicion.
      */
     public Tablero() {
-
         this.tablero = new Casilla[64];
         generaCasillas();
         generaPosiciones();
         calculaOrdenesMovimiento();
     }
 
-    private void generaPosiciones() {
-        //Recorre el array dando valor a posicion y point de cada tablero
-        for (int i = 1; i < 64; i++) {
-            //Calcula x e y temporales
-            int x;
-            if (Math.ceil(i / 9.0) % 2 != 0) {  //En las filas pares
-                x = (int) (20 + (100 * ((i - Math.floor((i - 1) / 9.0) * 9) - 1)));
-            } else {                    //En las filas impares
-                x = (int) (820 - (100 * ((i - Math.floor((i - 1) / 9.0) * 9) - 1)));
-            }
-            int y = (int) (715 - (100 * Math.ceil(i / 9.0)));  //Hay que corregir 5px hacia arriba (?)
-
-            //Da los valores buscados a tablero
-            tablero[i].setNumero(i);
-            tablero[i].setPosicionRelativa(new Point(x, y));
-        }
-    }
-
+    /**
+     * Recorre el Tablero generando en cada posicion la Casilla correspondiente
+     */
     private void generaCasillas() {
-        tablero[0] = new Casilla("salida");
-        //Recorre el array creando la tablero adecuada en cada posicion
         for (int i = 0; i < 64; i++) {
             switch (i) {
                 case 5:
@@ -62,45 +47,68 @@ public class Tablero {
                 case 50:
                 case 54:
                 case 59:
-                    tablero[i] = new Casilla("oca");
+                    tablero[i] = new Casilla(TipoCasilla.OCA);
                     break;
                 case 6:
                 case 12:
-                    tablero[i] = new Casilla("puente");
+                    tablero[i] = new Casilla(TipoCasilla.PUENTE);
                     break;
                 case 19:
-                    tablero[i] = new Casilla("posada");
+                    tablero[i] = new Casilla(TipoCasilla.POSADA);
                     break;
                 case 26:
                 case 53:
-                    tablero[i] = new Casilla("dados");
+                    tablero[i] = new Casilla(TipoCasilla.DADOS);
                     break;
-                case 31:
-                    tablero[i] = new Casilla("pozo");
+                case POS_POZO:
+                    tablero[i] = new Casilla(TipoCasilla.POZO);
                     break;
                 case 42:
-                    tablero[i] = new Casilla("laberinto");
+                    tablero[i] = new Casilla(TipoCasilla.LABERINTO);
                     break;
                 case 56:
-                    tablero[i] = new Casilla("carcel");
+                    tablero[i] = new Casilla(TipoCasilla.CARCEL);
                     break;
                 case 58:
-                    tablero[i] = new Casilla("calavera");
+                    tablero[i] = new Casilla(TipoCasilla.CALAVERA);
                     break;
                 case 63:
-                    tablero[i] = new Casilla("final");
+                    tablero[i] = new Casilla(TipoCasilla.JARDIN);
                     break;
                 default:
-                    tablero[i] = new Casilla();
+                    tablero[i] = new Casilla(TipoCasilla.NORMAL);
                     break;
             }
         }
     }
 
+    /**
+     * Recorre el Tablero asignando a cada Casilla sus posicionRelativa.
+     */
+    private void generaPosiciones() {
+        //Recorre el array dando valor a posicion y point de cada tablero
+        for (int i = 1; i < 64; i++) {
+            //Calcula x e y temporales (esquina superior izq de cada casilla
+            int x;
+            if (Math.ceil(i / 9.0) % 2 != 0) {  //En las filas pares
+                x = (int) (1 + (100 * ((i - Math.floor((i - 1) / 9.0) * 9) - 1)));
+            } else {                    //En las filas impares
+                x = (int) (801 - (100 * ((i - Math.floor((i - 1) / 9.0) * 9) - 1)));
+            }
+            int y = (int) (696 - (100 * Math.ceil(i / 9.0)));  //Hay que corregir 5px hacia arriba (?)
+
+            //Da los valores buscados a tablero
+            tablero[i].setNumero(i);
+            tablero[i].setPosicionRelativa(new Point(x, y));
+        }
+    }
+
+    /**
+     * Recorre el Tablero asignando a cada Casilla su movimientoOrden.
+     */
     private void calculaOrdenesMovimiento() {
         for (int i = 1; i < 64; i++) {
-            //Da movimientoOrden a las casillas oca, puente y dados
-            switch (tablero[i].getNombre()) {
+            switch (tablero[i].getTipoCasilla().getNombre()) {
                 case "oca":
                     tablero[i].setMovimientoOrden(nextOca(i));
                     break;
@@ -108,7 +116,13 @@ public class Tablero {
                     tablero[i].setMovimientoOrden(nextPosada(i));
                     break;
                 case "dados":
-                    tablero[i].setMovimientoOrden(nextDados(i));
+                    tablero[i].setMovimientoOrden(i);
+                    break;
+                case "laberinto":
+                    tablero[i].setMovimientoOrden(-12);
+                    break;
+                case "calavera":
+                    tablero[i].setMovimientoOrden(-57);
                     break;
             }
         }
@@ -124,7 +138,8 @@ public class Tablero {
         int contador = 0;
         do {
             contador++;
-        } while (i + contador <= 63 && !tablero[i + contador].getNombre().equals("oca"));
+        } while (!tablero[i + contador].getTipoCasilla().getNombre().equals("oca")
+                && !tablero[i + contador].getTipoCasilla().getNombre().equals("jardin"));
 
         return contador;
     }
@@ -139,51 +154,55 @@ public class Tablero {
         int contador = 0;
         do {
             contador++;
-        } while (!tablero[i + contador].getNombre().equals("posada"));
+        } while (!tablero[i + contador].getTipoCasilla().getNombre().equals("posada"));
 
         return contador;
     }
 
+//    /**
+//     * Metodo que cuenta el numero de casillas hasta tablero dados.
+//     *
+//     * @param i elemento de tablero desde el que empieza a contar.
+//     * @return n numero de casillas hasta la tablero dados.
+//     */
+//    public int nextDados(int i) {
+//        int contador = 0;
+//        boolean found = false;  //true cuando se ha encontrado el siguiente dado
+//
+//        //Busca siguiente tablero dados
+//        do {
+//            contador++;
+//            if (tablero[i + contador].getNombre().equals("dados")) {
+//                found = true;
+//            }
+//        } while (!found && (i + contador) < 63);
+//
+//        //Si la encuentra devuelve el contador
+//        if (found) {
+//            return contador;
+//
+//            //Si no la encuentra, busca la primera del tablero
+//        } else {
+//            contador = 0;
+//            do {
+//                contador++;
+//                if (tablero[contador].getNombre().equals("dados")) {
+//
+//                    //Si la encuentra devuelve la distancia a retroceder
+//                    return contador - i;
+//                }
+//            } while (contador < 63);
+//        }
+//        return 0;   //nunca deberia ejecutar est linea
+//    }
     /**
-     * Metodo que cuenta el numero de casillas hasta tablero dados.
+     * Devuelve la Casilla que ocupa una posicion del Tablero.
      *
-     * @param i elemento de tablero desde el que empieza a contar.
-     * @return n numero de casillas hasta la tablero dados.
+     * @param numeroCasilla
+     * @return Casilla que ocupa esa posicion en el Tablero
      */
-    public int nextDados(int i) {
-        int contador = 0;
-        boolean found = false;  //true cuando se ha encontrado el siguiente dado
-
-        //Busca siguiente tablero dados
-        do {
-            contador++;
-            if (tablero[i + contador].getNombre().equals("dados")) {
-                found = true;
-            }
-        } while (!found && (i + contador) < 63);
-
-        //Si la encuentra devuelve el contador
-        if (found) {
-            return contador;
-
-            //Si no la encuentra, busca la primera del tablero
-        } else {
-            contador = 0;
-            do {
-                contador++;
-                if (tablero[contador].getNombre().equals("dados")) {
-
-                    //Si la encuentra devuelve la distancia a retroceder
-                    return contador - i;
-                }
-            } while (contador < 63);
-        }
-        return 0;   //nunca deberia ejecutar est linea
-    }
-    
-    
-    public Casilla getCasilla(int i) {
-        return tablero[i];
+    public Casilla getCasilla(int numeroCasilla) {
+        return tablero[numeroCasilla];
     }
 
 }
